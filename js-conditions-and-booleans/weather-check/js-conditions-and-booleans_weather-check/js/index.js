dfc9d90b97e09a28c4f36bb3ecc4bb474382e2b5
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-06-01 16:00:37 +0200

    push

--- js-conditions-and-booleans/weather-check/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans/weather-check/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,21 +1,21 @@
 // Change this value to test different "weather" conditions.
 console.log("How is the weather today?");
-const weather = "cold";
+const weather = "sunny";
 
-switch (weather) {
-  case "rainy":
-    console.log("It's raining, don't forget your umbrella");
-    break;
-  case "sunny":
-    console.log("It's sunny!");
-    break;
-  case "snowy":
-    console.log("It's snowing, stay warm!");
-    break;
+//switch (weather) {
+//case "rainy":
+//console.log("It's raining, don't forget your umbrella");
+// break;
+//case "sunny":
+// console.log("It's sunny!");
+// break;
+//case "snowy":
+// console.log("It's snowing, stay warm!");
+//break;
 
-  default:
-    console.log("I'm not sure what the weather is like.");
-}
+//default:
+//console.log("I'm not sure what the weather is like.");
+//}
 
 // Adjust the "temperature" value to trigger different messages (for Part 2 only).
 const temperature = 22;
