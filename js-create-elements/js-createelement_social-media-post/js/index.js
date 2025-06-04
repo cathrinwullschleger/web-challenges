0fd792102ab37b91a15053d5734264bfd21e202c
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-06-04 17:46:22 +0200

    finish challenge

--- js-create-elements/js-createelement_social-media-post/js/index.js
+++ js-create-elements/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,31 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+const newArticle = document.createElement("article");
+newArticle.classList.add("post");
+
+const newP = document.createElement("p");
+newP.classList.add("post__content");
+newP.textContent =
+  "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.";
+
+const newFooter = document.createElement("footer");
+newFooter.classList.add("post__footer");
+
+const newUserName = document.createElement("span");
+newUserName.classList.add("post__username");
+newUserName.textContent = "@Michel";
+
+const newButton = document.createElement("button");
+newButton.classList.add("post__button");
+newButton.textContent = " ♥ Like";
+newButton.setAttribute("data-js", "like-button");
+newButton.type = "button";
+
+newButton.addEventListener("click", handleLikeButtonClick);
+
+newFooter.append(newUserName, newButton);
+newArticle.append(newP, newFooter);
+
+document.body.append(newArticle);
