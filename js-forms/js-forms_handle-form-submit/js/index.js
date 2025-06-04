99deecb3b3c8d86849eee5663f86c29493f89f77
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-06-04 11:29:27 +0200

    finish challenge

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,14 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+
+  const data = Object.fromEntries(formData.entries());
+  console.log("Formulardaten:", data);
+  event.target.reset();
+  event.target.querySelector('[name="firstName"]').focus();
+});
