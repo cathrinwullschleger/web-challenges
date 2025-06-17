f95e497c39a6ccea6eec916bf4811d7764dffd23
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-06-17 16:43:13 +0200

    finish button 2

--- react-props/react-props_buttons/src/App.jsx
+++ react-props/react-props_buttons/src/App.jsx
@@ -1,16 +1,30 @@
 export default function App() {
   return (
     <>
-      <Button color="pink" disabled text="Open" />
-      <Button color="#d6f804" disabled={false} text="close" />
-      <Button color="#69f4b6" disabled={false} text="submit" />
+      <Button color="pink" disabled={true} text="Open" />
+      <Button
+        color="#d6f804"
+        disabled={false}
+        text="close"
+        onClick={() => alert("You clicked me!")}
+      />
+      <Button
+        color="#69f4b6"
+        disabled={false}
+        text="submit"
+        onClick={() => console.log("Button clicked!")}
+      />
     </>
   );
 }
 
-function Button({ color, disabled, text }) {
+function Button({ color, disabled, text, onClick }) {
   return (
-    <button disabled={disabled} style={{ backgroundColor: color }}>
+    <button
+      disabled={disabled}
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+    >
       {text}
     </button>
   );
