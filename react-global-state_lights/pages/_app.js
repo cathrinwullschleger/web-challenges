f28e2cf82f24a7b6504e1745d8676ffc84b73dce
Cathrin Wullschleger <c.wullschleger@posteo.de>
2025-07-03 20:25:41 +0200

    counting lights, quick actions and dimming mode works

--- react-global-state_lights/pages/_app.js
+++ react-global-state_lights/pages/_app.js
@@ -22,10 +22,43 @@ export default function App({ Component, pageProps }) {
     );
   }
 
+  const lightsOnCount = (lights) => {
+    return lights.filter((light) => light.isOn).length;
+  };
+  function handleAllLightsOn() {
+    setLights((prevLights) => {
+      const updatedLights = prevLights.map((light) => ({
+        ...light,
+        isOn: true,
+      }));
+      console.log("All lights turned on:", updatedLights); // ✅ hier ist's gültig
+      return updatedLights;
+    });
+  }
+  function handleAllLightsOff() {
+    setLights((prevLights) =>
+      prevLights.map((light) => ({
+        ...light,
+        isOn: false,
+      }))
+    );
+  }
+
+  const isDimmed = (lights) => {
+    return lights.every((light) => !light.isOn);
+  };
   return (
-    <Layout>
+    <Layout isDimmed={isDimmed(lights)}>
       <GlobalStyle />
-      <Component {...pageProps} lights={lights} toggleLight={toggleLight} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        toggleLight={toggleLight}
+        handleAllLightsOn={handleAllLightsOn}
+        handleAllLightsOff={handleAllLightsOff}
+        lightsOnCount={lightsOnCount(lights)}
+        isDimmed={isDimmed(lights)}
+      />
     </Layout>
   );
 }
